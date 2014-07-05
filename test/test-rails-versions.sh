ce4d34fa8d918911dba7b9d3d86d5696c9624a63
James Friend <james@jsdf.co>
2014-07-05 21:08:10 +0800

    tests fix

--- test/test-rails-versions.sh
+++ test/test-rails-versions.sh
@@ -3,8 +3,8 @@
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
 rm $ROOT_DIR/Gemfile.lock
-RAILS_VERSION=3 bundle install --local
-RAILS_VERSION=3 bundle exec rake test
-rm $ROOT_DIR/Gemfile.lock
 RAILS_VERSION=4 bundle install --local
 RAILS_VERSION=4 bundle exec rake test
+rm $ROOT_DIR/Gemfile.lock
+RAILS_VERSION=3 bundle install --local
+RAILS_VERSION=3 bundle exec rake test
