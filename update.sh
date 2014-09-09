d5486403689d0b2c561bb1afd0d3e04323e29b08
James Friend <james@jsdf.co>
2014-09-09 15:28:32 +0800

    updated update script

--- update.sh
+++ update.sh
@@ -6,7 +6,9 @@ VERSION=`gem search "^$DEPENDENCY\$" --remote | egrep -o '[0-9]\.[0-9]\.[0-9]'`
 echo "updating to $VERSION"
 npm version $VERSION
 bundle install
-git commit -a -m "updated Gemfile.lock"
+if [ -n "$(git status --porcelain)" ]; then 
+  git commit -a
+fi
 rake test
 read -p "will publish $VERSION. are you sure? " -n 1 -r
 echo
