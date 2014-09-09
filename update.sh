38ec4b629dd6f3640c0c948fdd022feb270e7a61
James Friend <james@jsdf.co>
2014-09-09 15:22:58 +0800

    updated update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ VERSION=`gem search "^$DEPENDENCY\$" --remote | egrep -o '[0-9]\.[0-9]\.[0-9]'`
 echo "updating to $VERSION"
 npm version $VERSION
 bundle install
+git commit -a -m "updated Gemfile.lock"
 rake test
 read -p "will publish $VERSION. are you sure? " -n 1 -r
 echo
